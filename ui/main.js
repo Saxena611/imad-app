f21d0ff89e2e20c6bbb2b776432dea5217928cf6
Animesh Saxena <animeshsaxena611@gmail.com>
2018-03-08 21:59:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
-console.log('Loaded!');
+//working on counter
+
+var button  = document.getelementbyId('counter');
+var counter  = 0;
+button.onclick = function(){
+    //coonecting it to the end point
+    
+    //storing it in a variable and rendering it
+    
+    
+    //printing it in our span tag on clicking
+    counter = counter + 1;
+    var span = document.getelementbyId('count');
+    span.innerHTML = count.toString();
+}
